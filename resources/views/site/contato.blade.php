b8a2658397aea9539eedad4b7d8764b33bfd4e9e
sabrina <sabrinarfernandes.srf@gmail.com>
2023-02-28 05:46:23 -0300

    Menu Principal

--- resources/views/site/contato.blade.php
+++ resources/views/site/contato.blade.php
@@ -1,1 +1,13 @@
 <h3>Contato (view)</h3>
+
+<ul>
+    <li>
+        <a href="/">Principal</a>
+    </li>
+    <li>
+        <a href="/contato">Contato</a>
+    </li>
+    <li>
+        <a href="/sobreNos">Sobre Nos</a>
+    </li>
+</ul>
