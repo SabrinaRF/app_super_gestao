172d7a2d5e44d26c3363ea8c7c235f3011bf080f
sabrina <sabrinarfernandes.srf@gmail.com>
2023-03-01 17:12:43 -0300

    if e unless

--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -14,17 +14,30 @@
         <h3>Ainda não existem fornecedores cadastrados</h3>
     @endif
     */
+    /*
+    @if ($fornecedores[0]['status'] == 'N')
+        Fornecedor inativo
+    @endif
+    <br>
+    @unless($fornecedores[0]['status'] == 'S')
+        Fornecedor ativo
+    @endunless*/
+
+    if(isset($variavel)){
+
+    }
 @endphp
 
-
-Fornecedor:{{ $fornecedores[0]['nome'] }}<br>
-Status:{{ $fornecedores[0]['status'] }}<br>
-
-@if ($fornecedores[0]['status'] == 'N')
-    Fornecedor inativo
-@endif
-<br>
-@unless($fornecedores[0]['status'] == 'S')
-    Fornecedor ativo
-@endunless
+@isset($fornecedores)
+    Fornecedor:{{ $fornecedores[0]['nome'] }}<br>
+    Status:{{ $fornecedores[0]['status'] }}<br>
+    CNPJ:{{ $fornecedores[0]['cnpj'] }}<br>
+@endisset
+@isset($fornecedores)
+    Fornecedor:{{ $fornecedores[1]['nome'] }}<br>
+    Status:{{ $fornecedores[1]['status'] }}<br>
+    @isset($fornecedores[1]['cnpj'])
+        CNPJ:{{ $fornecedores[1]['cnpj'] }}<br>
+    @endisset
+@endisset
 
