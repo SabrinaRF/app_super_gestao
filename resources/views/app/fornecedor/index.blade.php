33409ff0bebf24d02f26021cbbf2c8d875f57d4c
sabrina <sabrinarfernandes.srf@gmail.com>
2023-03-01 19:12:50 -0300

    if e unless

--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -33,6 +33,7 @@
     Status:{{ $fornecedores[0]['status'] }}<br>
     CNPJ:{{ $fornecedores[0]['cnpj'] }}<br>
 @endisset
+
 @isset($fornecedores)
     Fornecedor:{{ $fornecedores[1]['nome'] }}<br>
     Status:{{ $fornecedores[1]['status'] }}<br>
