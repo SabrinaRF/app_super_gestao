214094bcc8111b704326eb47cc34e7a0c6fa2c5a
sabrina <sabrinarfernandes.srf@gmail.com>
2023-02-28 21:31:49 -0300

    comando if no blade

--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -1,3 +1,24 @@
 <h3>fornecedores</h3>
 
 {{-- fica o comentario --}}
+
+@php
+// o laravel entende que o @php como php puro.
+
+    /*if(){
+        
+    }else if(){
+
+    }else{
+
+    }*/
+
+@endphp
+
+@if (count($fornecedores) > 0 && count($fornecedores)< 10)
+    <h3>Existem alguns fornecedores cadastrados</h3>
+@elseif (count($fornecedores)> 10)
+    <h3>Existem vários fornecedores cadastrados</h3>
+@else
+    <h3>Ainda não existem fornecedores cadastrados</h3>
+@endif
