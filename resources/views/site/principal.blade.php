d8007bafeae2490368ede05b4e545a7ba68ee133
sabrina <sabrinarfernandes.srf@gmail.com>
2024-01-02 21:37:11 -0300

    Modificação na tela principal

--- resources/views/site/principal.blade.php
+++ resources/views/site/principal.blade.php
@@ -28,7 +28,7 @@
         <div class="direita">
             <div class="contato">
                 <h1>Contato</h1>
-                <p>Caso tenha qualquer dúvida por favor entre em contato com nossa equipe psazdxfcgvhbjn5drtfgyhuijelo formulário abaixo.<p>
+                <p>Caso tenha qualquer dúvida por favor entre em contato com nossa equipe pelo formulário abaixo.<p>
                 @component('site.layouts._components.form_contato',['classe'=>"borda-branca"])
                 @endcomponent
             </div>
