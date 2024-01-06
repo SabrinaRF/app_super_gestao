b78d5e554ea6df84ee4278e992a1553ee1c60be5
sabrina <sabrinarfernandes.srf@gmail.com>
2024-01-05 23:37:19 -0300

    Chaves estrangeiras (Relacionamento um para um)

---
+++ database/migrations/2024_01_05_143415_create_unidades_table.php
@@ -0,0 +1,54 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUnidadesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('unidades', function (Blueprint $table) {
+            $table->id();
+            $table->string('unidade',5);
+            $table->string('descricao',30);
+            $table->timestamps();
+        });
+
+        Schema::table('produtos', function (Blueprint $table){
+            $table->unsignedBigInteger('unidade_id');
+            $table->foreign('unidade_id')->references('id')->on('unidades');
+        });
+
+        Schema::table('produtos_detalhes', function(Blueprint $table){
+            $table->unsignedBigInteger('unidade_id');
+            $table->foreign('unidade_id')->references('id')->on('unidades');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('produto_detalhes', function (Blueprint $table){
+            $table->dropForeign('produto_detalhes_unidades_id_foreign');//[table]_[colunn]_foreign
+            $table->dropColumn('unidades_id');
+        });
+
+        Schema::table('produtos', function (Blueprint $table){
+            $table->dropForeign('produtos_unidades_id_foreing');
+            $table->dropColumn('unidades_id');
+
+        });
+
+        Schema::dropIfExists('unidades');
+    }
+}
