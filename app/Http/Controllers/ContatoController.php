a35ecfa0b5e9678982a838e522f96c71d240415f
sabrina <sabrinarfernandes.srf@gmail.com>
2024-01-09 22:17:17 -0300

    Validação de formlário

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -3,11 +3,56 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\SiteContato;
+use GuzzleHttp\Promise\Create;
 
 class ContatoController extends Controller
 {
-    public function contato(){
+    public function contato(Request $request){
 
-        return view('site.contato', ['titulo' => 'Contato']);
+        //print_r($request->all());
+
+        /*
+        $contato = new SiteContato();
+        $contato->name = $request->input('nome');
+        $contato->telefone = $request->input('telefone');
+        $contato->email = $request->input('email');
+        $contato->motivo_contato = $request->input('motivo_contato');
+        $contato->mensagem = $request->input('mensagem');
+
+        $contato->save();
+        
+        
+        $contato = new SiteContato();
+        $contato->fill($request->all());
+
+        dd($contato->getAttributes());
+            */
+       //$contato->save();
+
+       $motivo_contatos = [
+            '1'=> 'Dúvida',
+            '2'=> 'Elogio',
+            '3'=> 'Reclamação'
+       ];
+        return view('site.contato', ['titulo' => 'Contato','motivo_contatos' => $motivo_contatos ]);
+    }
+
+    public function salvar(Request $request){
+        
+        //validação dos dados
+        $request->validate([
+            'nome'=>'required|min:3|max:40',
+            'telefone'=> 'required',
+            'email'=> 'required',
+            'motivo_contato'=> 'required',
+            'mensagem'=> 'required|max:2000'
+        ]);
+
+
+
+
+
+        //SiteContato::create($request->all());
     }
 }
